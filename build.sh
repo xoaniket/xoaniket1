d978a3bf937470a320ea20b5935c306adb2854ad
xoaniket <cvinesh70@gmail.com>
2025-08-13 18:10:22 +0530

    Add automatic superuser creation for Render deploy

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+User.objects.filter(username='eren001').exists() or \
+User.objects.create_superuser('admin', 'eren2004@gmail.com', '1234567xo')" | python manage.py shell
