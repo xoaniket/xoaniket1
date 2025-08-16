2fc5dde1a096fdf43f8cac834f316b46fc44059a
xoaniket <cvinesh70@gmail.com>
2025-08-16 13:55:19 +0530

    Updated modified files

--- JOBPROJECT/settings.py
+++ JOBPROJECT/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']  # If you have a local static folder
+STATIC_URL = '/static/'
+  # If you have a local static folder
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   # Where collectstatic will store files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
