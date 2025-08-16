1d9b63834fffe08966f904872e7d2a44c2188ccc
xoaniket <cvinesh70@gmail.com>
2025-08-16 14:20:41 +0530

    Add build script for Render deployment

--- build.sh
+++ build.sh
@@ -9,3 +9,10 @@ python manage.py migrate --noinput
 
 # Collect static files
 python manage.py collectstatic --noinput
+
+echo "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='xo_aniket.888').exists():
+    User.objects.create_superuser('xo_aniket.888', 'xoaniket@example.com', 'areustupid')
+" | python manage.py shell
