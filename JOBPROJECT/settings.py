541e1ad76f003c95cb43b3d57b3da900047f4157
xoaniket <cvinesh70@gmail.com>
2025-08-16 12:17:47 +0530

    Updated modified files

--- JOBPROJECT/settings.py
+++ JOBPROJECT/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'JOBFORM' ,'static')]  # If you have a local static folder
+STATICFILES_DIRS = [BASE_DIR / 'static']  # If you have a local static folder
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   # Where collectstatic will store files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
