28d6f498e46020867067d44b533322f0a4c4a36c
xoaniket <cvinesh70@gmail.com>
2025-08-26 16:15:14 +0530

    Update settings.py

--- JOBPROJECT/settings.py
+++ JOBPROJECT/settings.py
@@ -115,7 +115,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = '/mnt/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
