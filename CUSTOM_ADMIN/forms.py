acbf476dd703487d37cce1f506341848157f260f
xoaniket <cvinesh70@gmail.com>
2025-08-30 18:53:34 +0530

    Updated this

--- CUSTOM_ADMIN/forms.py
+++ CUSTOM_ADMIN/forms.py
@@ -5,3 +5,14 @@ class JobPostForm(forms.ModelForm):
     class Meta:
         model = JobPost
         fields = '__all__'
+
+        widgets = {
+
+            'title':forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Title'}),
+            'company':forms.TextInput(attrs={'class':'form-control','placeholder':'Enter The Company Name'}),
+            'location':forms.TextInput(attrs={'class':'form-control'}),
+            'job_type':forms.TextInput(attrs={'class':'form-control'}),
+            'salary':forms.TextInput(attrs={'class':'form-control'}),
+            'description':forms.Textarea(attrs={'class':'form-control'}),
+            'requirements':forms.Textarea(attrs={'class':'form-control'}),
+        }
